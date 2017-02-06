566a7e52568e82eb73d1e81b641afa33e64c9897
hojohe <hojohe@nate.com>
2017-02-06 09:10:53 +0900

    2017.02.03

---
+++ 01_java-basic/src/test/Test03.java
@@ -0,0 +1,24 @@
+package test;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Test03 {
+	
+	
+	public static void main(String[] args) {
+		Date currentDate = new Date();
+		System.out.println("?? " + currentDate);
+
+	}
+
+	public Map<String, String> getDday(Date goalDate) {
+		
+		Map<String, String> dateMap = new HashMap<>();
+		
+		
+		
+		return dateMap;
+	}
+}
